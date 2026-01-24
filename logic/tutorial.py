208e57a9cf252ea7f6f231d310a9c6b0683b431b
Shuricato <antondrachev74@gmail.com>
2026-01-24 23:22:13 +0100

    Created tutorial window,

---
+++ logic/tutorial.py
@@ -0,0 +1,25 @@
+import sys
+import variables
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
+
+class tutorialWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("What is this program?")
+        self.setGeometry(350, 350, 600, 400)
+
+        main_widget = QWidget()
+        main_layout = QVBoxLayout()
+
+        #TODO: Tutorial
+        tutorial_label = QTextBrowser("Placeholder")
+        tutorial_label.setWordWrap(True)
+        main_layout.addWidget(tutorial_label)
+
+        close_btn = QPushButton("Close")
+        close_btn.clicked.connect(self.close)
+        main_layout.addWidget(close_btn)
+
+        main_widget.setLayout(main_layout)
+        self.setCentralWidget(main_widget)
