c643b6d8344b77a7fdb14728084494c3e06e1b7f
TC <tonydcrane@gmail.com>
2026-01-27 11:30:19 +0100

    new, per-file storage logic

--- logic/variables.py
+++ logic/variables.py
@@ -1,8 +1,12 @@
+from dataclasses import dataclass
+import datetime
 import sys
 import random
 import os
 import json
 from pathlib import Path
+import hashlib
+from typing import Dict, List
 
 _data = None
 _filebank = []
@@ -17,109 +21,142 @@ rank_weights = {
     5: 5
 }
 
-class Question:
-     def __init__(self, text, answers, correct_answers, source, num_correct):
-        self.text = text
-        self.answers = answers
-        self.correct_answers = correct_answers
-        self.source = source
-        self.num_correct = num_correct
-    #TODO: Question logic, file based ID
+@dataclass
+class metaFile:
+    filepath: Path
+    hash: str
+    filename: str
+    total_questions: int
+    last_updated: str
+    is_selected: bool
+
+@dataclass
+class metaQuestion:
+    id: str
+    text: str
+    source: str
+    rank: int
+    #Array for storing (question, value) tuples
+    questions = []
+
+class metaManager:
+    def __init__(self, questions_dir: str = "./questions"):
+        self.questions_dir = Path(questions_dir)
+        self.available: Dict[str, metaFile] = {}
+        self.loaded: Dict[str, List] = {}
+
+    #
+    def scan_files(self) -> List[metaFile]:
+        """
+        Looks for .md and .csv files in the questions directory, and parses them into the appropriate file objects
+        """
+
+        files = []
+        try:
+            for file in self.questions_dir:
+                if file.endswith(".md") or file.endswith(".csv"):
+                    file_meta = file.with_suffix('.meta.json')
+                    file_hash = self._get_file_hash(file, file_meta)
+                    metadata = self._load_or_create_meta(file, file_meta, file_hash)
+
+                    file_obj = metaFile(
+                        filepath = file,
+                        hash = file_hash,
+                        filename = file.name,
+                        total_questions = metadata['total_questions'],
+                        last_updated = metadata['last_updated']
+                    )
+                    files.append(file_obj)
+                    self.available[file_hash] = file_obj
+        except all:
+            print("Error during scanning")
+
+        return files
     
+    def get_hashes(filenames):
+        hashes = []
+        for file in filenames:
+            hashes.append(generate_file_hash(file.name))
+        return hashes
 
-def init(filename= "userdata.json"):
-    #Initialising both global variables
-    global _data
-    global _filebank
+    def select_files(self, filenames: List[str]):
+        """
+        Takes a list of filenames (ideally from main.py) and either enables them and/or loads the questions 
+        """
 
-    #All the logic of the program will execute locally to avoid clashing with the OneDrive storage
-    filepath = get_directory()/filename
+        hashes = self.get_hashes(filenames)
+        for hash in hashes:
+            if hash in self.available:
+                file_obj = self.available[hash]
+                file_obj.is_selected = True
+                
+                if hash not in self.loaded:
+                    self.loaded[hash] = self._parse_questions(file_obj)
 
-    try:
-        with open(filepath, 'r') as f:
-            _data = json.load(f)
-        print("Loading successful")
-    except FileNotFoundError:
-        print("File not found")
-        _data = make_file(filepath)
-    except json.JSONDecodeError:
-        print(f"Invalid Json")
-        _data = {}
+    def deselect_files(self, filenames: List[str]):
+        """
+        Disables question files and deloads their questions
+        """
+        hashes = self.get_hashes(filenames)
+        for hash in hashes:
+            if hash in self.available:
+                self.available[hash].is_selected = False
 
-    try:
-        for file in os.listdir("testfiles"):
-            if file.endswith(".md") or file.endswith(".csv"):
-                _filebank.append(file)
-    except all:
-        print("Error reading directory")
+                if hash in self.loaded:
+                    del self.loaded[hash]
 
-def update_questions(items):
-     _active_papers = items
-
-def get_questions():
-     return _active_papers
-
-def get_directory():
-    if getattr(sys, 'frozen', False):
-        localPath = Path(sys.executable).parent
-    else:
-        localPath = Path(__file__).parent
-    return localPath
-
-def make_file(filepath):
-    try:
-        with open(filepath, 'w') as f:
-            pass
-    except FileExistsError:
-        print("File already exists while trying to create {filepath}")
-
-def get_data():
-    if _data is None:
-        init()
-    return _data
-
-def get_filebank():
-    if _filebank.count == 0:
-        print("calling Init")
-        init()
-    return _filebank
-
-def save(filename= "userdata.json"):
-    filepath = get_directory()/filename
-    with open(filepath, 'w') as f:
-        json.dump(_data, f, indent=2)
-
-#TODO: find the question doc id in the user data, return the questions in a readable format
-def read_stats(item_ids):
-    for item_id in item_ids:
-        data = get_data()
+    def update_rank(self, hash: str, question_id: str, new_rank: int):
+        meta = self._get_meta(hash)
         
-#TODO: increment/decrement item id's associated rank by 1
-def up_rank(item_id):
-    pass
+        if meta.exists():
+            with open(meta, 'r') as f:
+                metadata = json.load(f)
+        #TODO: Else create file
 
-def down_rank(item_id):
-    pass
+        metadata['rankings'][question_id] = new_rank
+        metadata['last_updated'] = datetime.now().isoformat
 
-def pick_rank():
-    # Calculate total weights (100%)
-	total_weight = 0
-	for weight in rank_weights.values():
-		total_weight += weight
-	
-	# Pick a random value within the total weight
-    # random picks between 0.0 and 1.0, making it a % of 100
-	random_value = random.random() * total_weight
-	
-	# Use the accumulated value to determine the rank
-	cumulative_weight = 0
-	for rank in rank_weights.keys():
-		cumulative_weight += rank_weights.get(rank)
-		if random_value <= cumulative_weight:
-			return rank
+        with open(meta, 'w') as f:
+            json.dump(metadata, f, indent = 2)
 
-#TODO: fetch player data
-def pick_item(rank):
-    pass
+    def _get_file_hash(self, file:Path, meta:Path):
+        if meta.exists():
+            with open(file, 'r') as f:
+                return json.load(f)['hash']
+        hash = hashlib.md5(file.name.encode('utf-8'))
+        return hash.hexdigest()[:8]
+    
+    def _get_meta(self, hash: str):
+        file = self.available[hash]
+        return file.path.with_suffix('.meta.json')
 
-init()
+    def _load_ranks(self, meta: Path):
+        with open(meta, 'r') as f:
+            return json.load(f)['rankings']
+
+    def _load_or_create_meta(self, file: Path, meta: Path, hash: str):
+        if meta.exists():
+            with open(meta, 'r') as f:
+                return json.load(f)
+        
+        qcount = self._parse_question_count(file)
+
+        #All ranks in a new file are initiated as box 4 (id 1) so their odds are not the highest
+        metadata = {
+            "file_hash": hash,
+            "source_file": file.name,
+            "last_updated": datetime.now().isoformat(),
+            "total_questions": qcount,
+            "rankings": {f"{i+1:03d}": 1 for i in range(qcount)}
+        }
+
+        with open(meta, 'w') as f:
+            json.dump(metadata, f, indent=2)
+        
+        return metadata
+
+    def _parse_question_count(file):
+        pass
+
+    def _parse_questions(file: metaFile):
+        pass
